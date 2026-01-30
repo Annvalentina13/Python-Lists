531048e7a5554c95245b9f0d3a8c17ecca1285c6
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-30 21:33:39 +0530

    Create Attendance Tracker.py

---
+++ Attendance Tracker.py
@@ -0,0 +1,24 @@
+'''
+You are given a list where 1 means present and 0 means absent.
+
+attendance = [1, 0, 1, 1, 0, 1, 1]
+
+
+Task:
+
+Count total present students
+
+Count total absent students
+'''
+
+present = 0
+absent = 0
+
+for status in attendance:
+    if status == 1:
+        present += 1
+    else:
+        absent += 1
+
+print("Present:", present)
+print("Absent:", absent)
